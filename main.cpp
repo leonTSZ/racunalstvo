5e62b92844edb65106ecd683d0ded405bbffccf3
leonTSZ <55490492+leonTSZ@users.noreply.github.com>
2019-09-18 14:20:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+	float a;
+		cout<<"upisi temp : ";
+		cin>>a;
+		if (a>24,5)
+		{ 
+			cout<<"toplo";
+		}
+		else
+		{
+			cout<<"hladno ";
+		}
+		return 0;
+
+}
